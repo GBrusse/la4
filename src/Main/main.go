d6483c72086eaa6c51d940999b8185a883f7aa0d
GBrusse <gary.brusse@gmail.com>
2023-04-24 18:44:11 -0600

    no more people

--- src/Main/main.go
+++ src/Main/main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-	. "Main/sort"
-	. "fmt"
-	. "main/person"
+	"log"
+	"main/person"
 )
 
 func main() {
 	{
-		var pp IPerson = NewPerson("Ann", 21)
-		var ss IStudent = NewStudent("Bob", 22, "cs")
-		var ps IPerson = NewStudent("Carla", 23, "math")
-		Printf("pp.Name() => %s\n", pp.Name())
-		Printf("ss.Name() => %s\n", ss.Name())
-		Printf("pp.String() => %s\n", pp)
-		Printf("ss.String() => %s\n", ss)
-		Printf("ps.Name() => %s\n", ps.Name())
-		Printf("ps.String() => %s\n", ps)
+		var pp person.IPerson = person.NewPerson("Ann", 21)
+		var ss person.IStudent = person.NewStudent("Bob", 22, "cs")
+		var ps person.IPerson = person.NewStudent("Carla", 23, "math")
+		log.Printf("pp.Name() => %s\n", pp.Name())
+		log.Printf("ss.Name() => %s\n", ss.Name())
+		log.Printf("pp.String() => %s\n", pp)
+		log.Printf("ss.String() => %s\n", ss)
+		log.Printf("ps.Name() => %s\n", ps.Name())
+		log.Printf("ps.String() => %s\n", ps)
 	}
 	{
-		var pp *Person = NewPerson("Ann", 21)
-		var ss *Student = NewStudent("Bob", 22, "cs")
-		var ps IPerson = NewStudent("Carla", 23, "math")
-		Printf("pp.Name() => %s\n", pp.Name())
-		Printf("ss.Name() => %s\n", ss.Name())
-		Printf("pp.String() => %s\n", pp)
-		Printf("ss.String() => %s\n", ss)
-		Printf("ps.Name() => %s\n", ps.Name())
-		Printf("ps.String() => %s\n", ps)
+		var pp *person.Person = person.NewPerson("Ann", 21)
+		var ss *person.Student = person.NewStudent("Bob", 22, "cs")
+		var ps person.IPerson = person.NewStudent("Carla", 23, "math")
+		log.Printf("pp.Name() => %s\n", pp.Name())
+		log.Printf("ss.Name() => %s\n", ss.Name())
+		log.Printf("pp.String() => %s\n", pp)
+		log.Printf("ss.String() => %s\n", ss)
+		log.Printf("ps.Name() => %s\n", ps.Name())
+		log.Printf("ps.String() => %s\n", ps)
 	}
 	{
-		pp := NewPerson("Ann", 21)
-		ss := NewStudent("Bob", 22, "cs")
-		ps := NewStudent("Carla", 23, "math")
-		Printf("pp.Name() => %s\n", pp.Name())
-		Printf("ss.Name() => %s\n", ss.Name())
-		Printf("pp.String() => %s\n", pp)
-		Printf("ss.String() => %s\n", ss)
-		Printf("ps.Name() => %s\n", ps.Name())
-		Printf("ps.String() => %s\n", ps)
+		pp := person.NewPerson("Ann", 21)
+		ss := person.NewStudent("Bob", 22, "cs")
+		ps := person.NewStudent("Carla", 23, "math")
+		log.Printf("pp.Name() => %s\n", pp.Name())
+		log.Printf("ss.Name() => %s\n", ss.Name())
+		log.Printf("pp.String() => %s\n", pp)
+		log.Printf("ss.String() => %s\n", ss)
+		log.Printf("ps.Name() => %s\n", ps.Name())
+		log.Printf("ps.String() => %s\n", ps)
 	}
 }
